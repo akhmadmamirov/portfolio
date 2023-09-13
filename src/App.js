125774418dfd9959f35ffe5b94b8217c80ddb0fb
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-09-13 18:33:22 -0400

    removing intro message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import ReactGA from "react-ga4";
 
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
@@ -9,37 +7,12 @@ import Experiences from "./pages/experience";
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
 
-import { TRACKING_ID } from "./data/tracking";
 import "./app.css";
 
 function App() {
-	const [showIntro, setShowIntro] = useState(true);
-	useEffect(() => {
-		if (TRACKING_ID !== "") {
-			ReactGA.initialize(TRACKING_ID);
-		}
-		setTimeout(() => {
-			setShowIntro(false);
-		  }, 4200);
-	}, []);
-
 	return (
 		<div className="App">
-			<div>
-			{showIntro ? (
-			<div className="loading-container">
-				<div class="loading-text-container">
-					<div class="text primary-text">
-						<p> Hello, my name is <span class="name">Akhmadillo</span>.</p>
-						</div>
-						<div>
-					</div>
-					<div class="text secondary-text">
-						<p >Welcome to my portolio site!</p>
-					</div>
-				</div>
-			</div>
-		) : (
+			
 			<div>
 				<Routes>
 					<Route path="/" element={<Homepage />} />
@@ -50,10 +23,9 @@ function App() {
 					<Route path="*" element={<Notfound />} />
 				</Routes>
 			</div>
-		)}
-    </div>
-		</div>
-	);
+		
+    	</div>
+		)	
 }
 
 export default App;
