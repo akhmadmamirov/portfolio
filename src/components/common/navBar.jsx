5f32cb09ee7ad238376808fa171b16ebe00c02ee
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-09-13 18:23:42 -0400

    adjusting to mobile view

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -1,68 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
 import "./styles/navBar.css";
 
 const NavBar = (props) => {
-	const { active } = props;
+  const { active } = props;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-	return (
-		<React.Fragment>
-			<div className="nav-container">
-				<nav className="navbar">
-					<div className="nav-background">
-						<ul className="nav-list">
-							<li
-								className={
-									active === "home"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/">Home</Link>
-							</li>
-							<li
-								className={
-									active === "about"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/about">About</Link>
-							</li>
-							<li
-								className={
-									active === "projects"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/projects">Projects</Link>
-							</li>
-							<li
-								className={
-									active === "experiences"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/experience">Experience</Link>
-							</li>
-							<li
-								className={
-									active === "contact"
-										? "nav-item active"
-										: "nav-item"
-								}
-							>
-								<Link to="/contact">Contact</Link>
-							</li>
-						</ul>
-					</div>
-				</nav>
-			</div>
-		</React.Fragment>
-	);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <React.Fragment>
+      <div className="nav-container">
+        <nav className={`navbar ${isMenuOpen ? "active" : ""}`}>
+          <div className="nav-background">
+            <div className="menu-icon" onClick={toggleMenu}>
+              {isMenuOpen ? (
+                <span className="material-icons material-symbols-outlined close-button">close</span>
+              ) : (
+                <span className="material-icons material-symbols-outlined menu-button">menu</span>
+              )}
+            </div>
+            <ul className={`nav-list ${isMenuOpen ? "active" : ""}`}>
+              <li
+                className={
+                  active === "home" ? "nav-item active" : "nav-item"
+                }
+              >
+                <Link to="/" onClick={toggleMenu}>
+                  Home
+                </Link>
+              </li>
+              <li
+                className={
+                  active === "about" ? "nav-item active" : "nav-item"
+                }
+              >
+                <Link to="/about" onClick={toggleMenu}>
+                  About
+                </Link>
+              </li>
+              <li
+                className={
+                  active === "projects" ? "nav-item active" : "nav-item"
+                }
+              >
+                <Link to="/projects" onClick={toggleMenu}>
+                  Projects
+                </Link>
+              </li>
+              <li
+                className={
+                  active === "experience" ? "nav-item active" : "nav-item"
+                }
+              >
+                <Link to="/experience" onClick={toggleMenu}>
+                  Experience
+                </Link>
+              </li>
+              <li
+                className={
+                  active === "contact" ? "nav-item active" : "nav-item"
+                }
+              >
+                <Link to="/contact" onClick={toggleMenu}>
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default NavBar;
