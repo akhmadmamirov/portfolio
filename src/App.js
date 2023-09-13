6e6bf1b4524484861babee9d1827b688b49e5242
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-09-13 11:08:30 -0400

    adding loading-text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4";
 
@@ -13,22 +13,45 @@ import { TRACKING_ID } from "./data/tracking";
 import "./app.css";
 
 function App() {
+	const [showIntro, setShowIntro] = useState(true);
 	useEffect(() => {
 		if (TRACKING_ID !== "") {
 			ReactGA.initialize(TRACKING_ID);
 		}
+		setTimeout(() => {
+			setShowIntro(false);
+		  }, 4200);
 	}, []);
 
 	return (
 		<div className="App">
-			<Routes>
-				<Route path="/" element={<Homepage />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/projects" element={<Projects />} />
-				<Route path="/experience" element={<Experiences />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="*" element={<Notfound />} />
-			</Routes>
+			<div>
+			{showIntro ? (
+			<div className="loading-container">
+				<div class="loading-text-container">
+					<div class="text primary-text">
+						<p> Hello, my name is <span class="name">Akhmadillo</span>.</p>
+						</div>
+						<div>
+					</div>
+					<div class="text secondary-text">
+						<p >Welcome to my portolio site!</p>
+					</div>
+				</div>
+			</div>
+		) : (
+			<div>
+				<Routes>
+					<Route path="/" element={<Homepage />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/experience" element={<Experiences />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="*" element={<Notfound />} />
+				</Routes>
+			</div>
+		)}
+    </div>
 		</div>
 	);
 }
