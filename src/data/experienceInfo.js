97f311afde9c158f829ee7d42863a48584b23515
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2024-09-10 16:47:02 -0400

    adding new experience view

---
+++ src/data/experienceInfo.js
@@ -0,0 +1,53 @@
+const experienceINFO = {
+  experience: [
+    {
+      company: "Electronic Arts (EA)",
+      title: "Software Engineer Intern",
+      date: "05/20/2024 - 08/09/2024",
+      points: [
+        "Developed and onboarded a new Account Management Tool for seamless troubleshooting of Nucleus player info.", 
+        "Created a centralized platform with React, and Typescript for player ownership, subscriptions, and entitlements.",
+        "Designed and implemented 25+ secure Node.js APIs consuming EA internal APIs for Identity, Commerce, and Audit services. Consistently exceeded expectations with fast and reliable delivery of new functionalities.",
+        "Worked with 5+ teams to Integrate OAuth, Player Infraction, Gateway APIs, providing secure and scalable solution with enhanced access control.",
+        "Deployed into an integration environment using Vault for secrets, Jenkins, Docker for CI/CD, with custom logging and monitoring features. Built on AWS (S3, EC2) with Terraform."
+      ],
+      skills: ["Java", "JavaScript", "Docker", "React", "Express", "Jenkins"]
+    },
+    {
+      company: "Google Developer Student Clubs",
+      title: "Founder & Lead",
+      date: "08/10/2023 - Present",
+      points: [
+        "Established and spearheaded the first Google Developer Student Clubs (GDSC) at the College of Wooster (COW) to create an environment for members to learn, support, and network with each other.",
+        "Coordinated logistics between multiple parties to create beneficial events such as Coding Workshops and Tech Talks.",
+        "Worked directly with the chair of CS & Math department to expand the GDSC mission, reaching 90% of students within the department and extending its influence to other academic departments at the COW."
+      ],
+      skills: ["Leadership", "Communication", "Team Work", "Networking"]
+    },
+    {
+      company: "AtomRain Inc",
+      title: "Software Engineer Intern",
+      date: "05/10/2023 - 07/05/2023",
+      points: [
+        "Optimized UI Performance by refining React and Redux UI components (Security, Payment, Billing) for the GraphGrid CDP, resulting in 25% faster load times and a 40% reduction in user-reported glitches.",
+        "Enhanced Amazon EC2 instance types by revamping grid configurations in the React UI, and Node.js microservices,increasing resource allocation efficiency by 30% and reducing the network latency by 20%.",
+        "Boosted scalability and codebase stability by leading upgrades of Node.js and React across the codebase, resulting in 5x heightened scalability and code maintainability, while also reducing runtime errors by 70%."
+      ],
+      skills: ["React.js", "Node.js", "JavaScript", "SASS", "HTML", "AWS", "Redux", "API Testing"]
+    },
+    {
+      company: "The College of Wooster",
+      title: "AI Research Intern",
+      date: "10/19/2023 - 05/07/2024",
+      points: [
+        "Created a chat solution answering queries across 200+ College of Wooster websites with natural language recognition, helping 2000+ parents and returning over 1000 response queries a month.",
+        "Developed an NLP pipeline using OpenAI API and Langchain, automating 100% of MySQL query generation.",
+        "Achieved 200% total traffic throughput by orchestrating 5+ microservices using Express.js and AWS (EC2, S3).",
+        "Authored 2 research papers on LLMs and Distributed Systems; mentored a new intern on prompt engineering."
+      ],
+      skills: ["OpenAI API", "Langchain", "Pinecone", "AWS", "Prompt Engineerings"]
+    }
+  ]
+}
+
+export default experienceINFO
